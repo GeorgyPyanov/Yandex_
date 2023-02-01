968b4ae1b949dd6d9ef5ba216d1150640411ebe5
Георгий <georgpyanov@gmail.com>
2023-02-01 23:32:10 +0300

    Поиск аптеки 2.0

--- main.py
+++ main.py
@@ -1,6 +1,27 @@
+import math
 import sys
 import requests
-from we import get_ll_span, start
+from we import start
+
+
+def lonlat_distance(a, b):
+
+    degree_to_meters_factor = 111 * 1000 # 111 километров в метрах
+    a_lon, a_lat = a
+    b_lon, b_lat = b
+
+    # Берем среднюю по широте точку и считаем коэффициент для нее.
+    radians_lattitude = math.radians((a_lat + b_lat) / 2.)
+    lat_lon_factor = math.cos(radians_lattitude)
+
+    # Вычисляем смещения в метрах по вертикали и горизонтали.
+    dx = abs(a_lon - b_lon) * degree_to_meters_factor * lat_lon_factor
+    dy = abs(a_lat - b_lat) * degree_to_meters_factor
+
+    # Вычисляем расстояние между точками.
+    distance = math.sqrt(dx * dx + dy * dy)
+
+    return distance
 
 
 toponym_to_find = " ".join(sys.argv[1:])
@@ -15,12 +36,30 @@ toponym = json_response["response"]["GeoObjectCollection"][
     "featureMember"][0]["GeoObject"]
 toponym_coodrinates = toponym["Point"]["pos"]
 toponym_to_find = " ".join(sys.argv[1:])
+search_api_server = "https://search-maps.yandex.ru/v1/"
+api_key = "dda3ddba-c9ea-4ead-9010-f43fbc15c6e3"
+address_ll = ','.join(toponym_coodrinates.split())
+search_params = {
+    "apikey": api_key,
+    "text": "аптека",
+    "lang": "ru_RU",
+    "ll": address_ll,
+    "type": "biz"
+}
+response = requests.get(search_api_server, params=search_params)
+json_response = response.json()
+organization = json_response["features"][0]
+org_name = organization["properties"]["CompanyMetaData"]["name"]
+org_address = organization["properties"]["CompanyMetaData"]["address"]
+point = organization["geometry"]["coordinates"]
+time = organization["properties"]["CompanyMetaData"]['Hours']['text']
+org_point = "{0},{1}".format(point[0], point[1])
 map_params = {
-    "ll": get_ll_span(toponym_to_find)[0],
-    "spn": get_ll_span(toponym_to_find)[1],
     "l": "map",
-    'pt': ','.join(toponym_coodrinates.split())
+    'pt': '~'.join([','.join(toponym_coodrinates.split()), org_point])
 }
 map_api_server = "http://static-maps.yandex.ru/1.x/"
 response = requests.get(map_api_server, params=map_params)
+metrs = lonlat_distance(point, list(map(float, toponym_coodrinates.split())))
+print([org_name, org_address, time, metrs])
 start(response)
