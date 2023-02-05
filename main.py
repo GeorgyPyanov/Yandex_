035cf21ccb9d4b03b8e2f3fb0386fbfae5519cb1
Георгий <georgpyanov@gmail.com>
2023-02-05 17:39:05 +0300

    Полный поиск

--- main.py
+++ main.py
@@ -1,86 +1,106 @@
-import math
+import os
 import sys
+from random import choice, randrange
+import pygame
 import requests
-from we import start
 
 
-def lonlat_distance(a, b):
-
-    degree_to_meters_factor = 111 * 1000 # 111 километров в метрах
-    a_lon, a_lat = a
-    b_lon, b_lat = b
-
-    # Берем среднюю по широте точку и считаем коэффициент для нее.
-    radians_lattitude = math.radians((a_lat + b_lat) / 2.)
-    lat_lon_factor = math.cos(radians_lattitude)
-
-    # Вычисляем смещения в метрах по вертикали и горизонтали.
-    dx = abs(a_lon - b_lon) * degree_to_meters_factor * lat_lon_factor
-    dy = abs(a_lat - b_lat) * degree_to_meters_factor
-
-    # Вычисляем расстояние между точками.
-    distance = math.sqrt(dx * dx + dy * dy)
-
-    return distance
+a = ['Азнакаево', 'Альметьевск', 'Арск', 'Бавлы', '	Болгар', 'Бугульма', 'Буинск',
+     'Елабуга', 'Заинск', '	Зеленодольск', 'Иннополис', 'Казань',
+     'Кукмор', 'Лаишево', 'Нижнекамск', 'Набережные Челны', 'Нурлат', 'Чистополь']
+API_KEY = '40d1649f-0493-4b70-98ba-98533de7710b'
 
 
-toponym_to_find = " ".join(sys.argv[1:])
-geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
-geocoder_params = {
-    "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
-    "geocode": toponym_to_find,
-    "format": "json"}
-response = requests.get(geocoder_api_server, params=geocoder_params)
-json_response = response.json()
-toponym = json_response["response"]["GeoObjectCollection"][
-    "featureMember"][0]["GeoObject"]
-toponym_coodrinates = toponym["Point"]["pos"]
-toponym_to_find = " ".join(sys.argv[1:])
-search_api_server = "https://search-maps.yandex.ru/v1/"
-api_key = "dda3ddba-c9ea-4ead-9010-f43fbc15c6e3"
-address_ll = ','.join(toponym_coodrinates.split())
-search_params = {
-    "apikey": api_key,
-    "text": "аптека",
-    "lang": "ru_RU",
-    "ll": address_ll,
-    "type": "biz"
-}
-response = requests.get(search_api_server, params=search_params)
-json_response = response.json()
-o = [','.join(toponym_coodrinates.split())]
-if len(json_response["features"]) > 10:
-    n = 10
-else:
-    n = len(json_response["features"])
-for i in range(n):
-    organization = json_response["features"][i]
-    org_name = organization["properties"]["CompanyMetaData"]["name"]
-    org_address = organization["properties"]["CompanyMetaData"]["address"]
-    point = organization["geometry"]["coordinates"]
-    try:
-        if organization["properties"]["CompanyMetaData"]['Hours']['Availabilities'][0]['TwentyFourHours']:
-            time = 2
-    except:
-        time = 3
-    if time == 3:
-        try:
-            a = organization["properties"]["CompanyMetaData"]['Hours']
-            time = 1
-        except:
-            time = 3
-    if time == 3:
-        r = 'pmgrs'
-    elif time == 2:
-        r = 'pmgns'
+def geocode(address):
+    geocoder_request = f"http://geocode-maps.yandex.ru/1.x/?apikey={API_KEY}" \
+                       f"&geocode={address}&format=json"
+    response = requests.get(geocoder_request)
+    if response:
+        json_response = response.json()
     else:
-        r = 'pmbls'
-    org_point = "{0},{1}".format(point[0], point[1]) + f',{r}'
-    o.append(org_point)
-map_params = {
-    "l": "map",
-    'pt': '~'.join(o)
-}
-map_api_server = "http://static-maps.yandex.ru/1.x/"
-response = requests.get(map_api_server, params=map_params)
-start(response)
+        raise RuntimeError(
+            """Ошибка выполнения запроса:
+                       {request}
+                Http статус: {status} ({reason})""".format(
+                request=geocoder_request, status=response.status_code, reason=response.reason))
+    features = json_response["response"]["GeoObjectCollection"]["featureMember"]
+    return features[0]["GeoObject"] if features else None
+
+
+def get_ll_span(address):
+    toponym = geocode(address)
+    if not toponym:
+        return (None, None)
+    toponym_coodrinates = toponym["Point"]["pos"]
+    toponym_longitude, toponym_lattitude = toponym_coodrinates.split(" ")
+    ll = ",".join([toponym_longitude, toponym_lattitude])
+    envelope = toponym["boundedBy"]["Envelope"]
+    l, b = envelope["lowerCorner"].split(" ")
+    r, t = envelope["upperCorner"].split(" ")
+    dx = abs(float(l) - float(r)) / 2.0
+    dy = abs(float(t) - float(b)) / 2.0
+    span = f"{dx},{dy}"
+    return ll, span
+
+
+def start(response, toponym_to_find):
+    if not response:
+        print("Ошибка выполнения запроса:")
+        print("Http статус:", response.status_code, "(", response.reason, ")")
+        sys.exit(1)
+    map_file = "map.png"
+    with open(map_file, "wb") as file:
+        file.write(response.content)
+    pygame.init()
+    screen = pygame.display.set_mode((600, 450))
+    screen.blit(pygame.image.load(map_file), (0, 0))
+    pygame.display.flip()
+    running = True
+    n = 0
+    pygame.mixer.music.load("233843.mp3")
+    pygame.mixer.music.play(-1)
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                n = 1
+                font = pygame.font.Font(None, 20)
+                text = font.render(toponym_to_find, True, (randrange(255), randrange(255), randrange(255)))
+        if n == 100:
+            os.remove(map_file)
+            toponym_to_find = choice(a)
+            response = mama(toponym_to_find)
+            if not response:
+                print("Ошибка выполнения запроса:")
+                print("Http статус:", response.status_code, "(", response.reason, ")")
+                sys.exit(1)
+            map_file = "map.png"
+            with open(map_file, "wb") as file:
+                file.write(response.content)
+            n = 0
+        screen.blit(pygame.image.load(map_file), (0, 0))
+        if n > 0:
+            font = pygame.font.Font(None, 80)
+            text = font.render(toponym_to_find, True, (randrange(255), randrange(255), randrange(255)))
+            screen.blit(text, (0, 300))
+            n += 1
+        pygame.display.flip()
+    pygame.quit()
+    os.remove(map_file)
+
+
+def mama(toponym_to_find):
+    map_params = {
+        "ll": get_ll_span(toponym_to_find)[0],
+        "spn": get_ll_span(toponym_to_find)[1],
+        "l": "sat"
+    }
+    map_api_server = "http://static-maps.yandex.ru/1.x/"
+    response = requests.get(map_api_server, params=map_params)
+    return response
+
+
+if __name__ == '__main__':
+    toponym_to_find = choice(a)
+    start(mama(toponym_to_find), toponym_to_find)
