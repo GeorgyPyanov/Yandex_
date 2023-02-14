fe46ab38b30ba806a7dbeed14f59fa22ff07af5e
Георгий <georgpyanov@gmail.com>
2023-02-14 23:53:14 +0300

    Загрузка файла

--- __init__.py
+++ __init__.py
@@ -66,7 +66,32 @@ def sample_file_upload():
     elif request.method == 'POST':
         f = request.files['file']
         f.save('static/img/1' + f.filename[-4:])
-        return f'''<img src="static/img/1{f.filename[-4:]}" alt="здесь должна была быть картинка, но не нашлась">'''
+        return f'''<!doctype html>
+                        <html lang="en">
+                          <head>
+                            <meta charset="utf-8">
+                            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                             <link rel="stylesheet"
+                             href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+                             integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+                             crossorigin="anonymous">
+                            <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
+                            <title>Пример загрузки файла</title>
+                          </head>
+                          <body>
+                            <h1>Загрузим файл</h1>
+                            <div>
+                              <form class="login_form" method="post" enctype="multipart/form-data">
+                                <div class="form-group">
+                                  <label for="photo">Приложите фотографию</label>
+                                  <input type="file" class="form-control-file" id="photo" name="file">
+                                  <img src="static/img/1{f.filename[-4:]}" alt="здесь должна была быть картинка, но не нашлась">
+                                </div>
+                                <button type="submit" class="btn btn-primary">Записаться</button>
+                              </form>
+                            </div>
+                          </body>
+                        </html>'''
 
 
 if __name__ == '__main__':
